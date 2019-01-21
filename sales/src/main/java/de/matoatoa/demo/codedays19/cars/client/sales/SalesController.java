cc44274f6c3ad3d647a0012d68949437ce4ddf67
Jan Hauer <Jan.Hauer@EXXETA.com>
2019-01-21 11:19:03 +0100

    07: SalesController

---
+++ sales/src/main/java/de/matoatoa/demo/codedays19/cars/client/sales/SalesController.java
@@ -0,0 +1,53 @@
+package de.matoatoa.demo.codedays19.cars.client.sales;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * @author Jan Hauer
+ */
+@RestController
+@RequestMapping("/vehicles")
+public class SalesController {
+
+    private static final Predicate<Vehicle> NOT_SOLD = ((Predicate<Vehicle>) Vehicle::isSold).negate();
+    private final VehicleService vehicleService;
+    private final CustomerService customerService;
+
+    public SalesController(VehicleService vehicleService, CustomerService customerService) {
+        this.vehicleService = vehicleService;
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/")
+    public List<Vehicle> getVehicles() {
+        return vehicleService.getAll();
+    }
+
+    @GetMapping("/available")
+    public List<Vehicle> getAvailableVehicles() {
+        return getVehicles().parallelStream() //
+                .filter(NOT_SOLD) //
+                .collect(Collectors.toList());
+    }
+
+    @PutMapping("/sale/{vin}")
+    public void sale(@PathVariable String vin, @Valid @RequestBody Customer customer) {
+        customerService.createOrUpdate(customer);
+        vehicleService.sale(vin, customer);
+    }
+
+    @GetMapping("/owner/{vin}")
+    public Customer getOwner(@PathVariable String vin) {
+        return vehicleService.getOwner(vin);
+    }
+}
