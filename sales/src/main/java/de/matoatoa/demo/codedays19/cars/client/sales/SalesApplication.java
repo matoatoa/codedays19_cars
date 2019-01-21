aa9606fb83c3fd39b04c0a277d64d7d8cd7a459c
Jan Hauer <Jan.Hauer@EXXETA.com>
2019-01-21 10:11:28 +0100

    02: Sales ergänzen

---
+++ sales/src/main/java/de/matoatoa/demo/codedays19/cars/client/sales/SalesApplication.java
@@ -0,0 +1,14 @@
+package de.matoatoa.demo.codedays19.cars.client.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesApplication.class, args);
+    }
+
+}
+
