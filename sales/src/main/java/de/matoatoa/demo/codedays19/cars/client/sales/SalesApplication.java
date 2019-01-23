0f10b5cdd2337e8b0c7588d0a81bf563f2ddf118
Jan Hauer <Jan.Hauer@EXXETA.com>
2019-01-23 21:58:35 +0100

    18: Adds Beans

--- sales/src/main/java/de/matoatoa/demo/codedays19/cars/client/sales/SalesApplication.java
+++ sales/src/main/java/de/matoatoa/demo/codedays19/cars/client/sales/SalesApplication.java
@@ -1,5 +1,9 @@
 package de.matoatoa.demo.codedays19.cars.client.sales;
 
+import de.matoatoa.demo.codedays19.cars.client.common.CarsExceptionHandler;
+import de.matoatoa.demo.codedays19.cars.client.common.CustomerController;
+import de.matoatoa.demo.codedays19.cars.client.common.CustomerService;
+import de.matoatoa.demo.codedays19.cars.client.common.VehicleService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -30,6 +34,26 @@ public class SalesApplication {
                 .basicAuthentication(user, password) //
                 .rootUri(url) //
                 .build();
+    }
+
+    @Bean
+    public CustomerService customerService(RestTemplate restTemplate) {
+        return new CustomerService(restTemplate);
+    }
+
+    @Bean
+    public VehicleService vehicleService(RestTemplate restTemplate) {
+        return new VehicleService(restTemplate);
+    }
+
+    @Bean
+    public CustomerController customerController(CustomerService customerService) {
+        return new CustomerController(customerService);
+    }
+
+    @Bean
+    public CarsExceptionHandler carsExceptionHandler() {
+        return new CarsExceptionHandler();
     }
 
     public static void main(String[] args) {
