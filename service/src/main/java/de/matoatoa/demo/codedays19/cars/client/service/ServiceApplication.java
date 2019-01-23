816d3f8976b7e38ea96b0570104a8fed7aabcd8c
Jan Hauer <Jan.Hauer@EXXETA.com>
2019-01-24 00:11:50 +0100

    09: Zweiter Client

---
+++ service/src/main/java/de/matoatoa/demo/codedays19/cars/client/service/ServiceApplication.java
@@ -0,0 +1,13 @@
+package de.matoatoa.demo.codedays19.cars.client.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+
+}
+
