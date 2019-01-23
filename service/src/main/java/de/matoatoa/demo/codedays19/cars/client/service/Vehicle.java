e2057163d812af409a940f9580033be5c9318079
Jan Hauer <Jan.Hauer@EXXETA.com>
2019-01-23 21:22:17 +0100

    12: Models

---
+++ service/src/main/java/de/matoatoa/demo/codedays19/cars/client/service/Vehicle.java
@@ -0,0 +1,45 @@
+package de.matoatoa.demo.codedays19.cars.client.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Wither;
+import org.springframework.lang.Nullable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+/**
+ * @author Jan Hauer
+ */
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Wither
+public class Vehicle {
+
+    @NotNull
+    private String vin;
+
+    @Nullable
+    private Customer owner;
+
+    @NotBlank
+    private String vehicleModel;
+
+    @NotNull
+    private LocalDate dateOfConstruction;
+
+    @Nullable
+    private LocalDate dateOfPurchase;
+
+    @Nullable
+    private LocalDate dateOfLastService;
+
+    public boolean hadService() {
+        return dateOfLastService != null;
+    }
+}
